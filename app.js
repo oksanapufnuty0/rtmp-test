d04a60c4b0d5ee7fc939de30b576bec6afd8a5fd
oksanapufnuty0 <jinzengguo107@gmail.com>
2021-01-12 04:40:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express'),
+    Session = require('express-session'),
+    bodyParse = require('body-parser'),
+    mongoose = require('mongoose'),
+    middleware = require('connect-ensure-login'),
+    FileStore = require('session-file-store')(Session),
+    config = require('./config/default'),
+    flash = require('connect-flash'),
+    port = 3333,
+    app = express();
+ 
+mongoose.connect('mongodb://127.0.0.1/nodeStream' , { useNewUrlParser: true });
+
+// Add on the top next to imports
+const passport = require('./auth/passport');
+ 
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './views'));
+app.use(express.static('public'));
+app.use(flash());
+app.use(require('cookie-parser')());
+app.use(bodyParse.urlencoded({extended: true}));
+app.use(bodyParse.json({extended: true}));
+ 
+app.use(Session({
+    store: new FileStore({
+        path : './server/sessions'
+    }),
+    secret: config.server.secret,
+    maxAge : Date().now + (60 * 1000 * 30)
+}));
+ 
+app.get('*', middleware.ensureLoggedIn(), (req, res) => {
+    res.render('index');
+});
+
+// Register app routes
+
+app.use('/login', require('./routes/login'));
+app.use('/register', require('./routes/register'));
+
+app.listen(port, () => console.log(`App listening on ${port}!`));
